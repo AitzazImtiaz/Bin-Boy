1ba8ae8e16f0f8f7daecd10e305192e637930c99
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-03-26 17:13:59 +0500

    Update __main__.py

--- aitbin/__main__.py
+++ aitbin/__main__.py
@@ -1,5 +1,5 @@
 from colorama import Fore
 def main():
   while True:
-    print(Fore.WHITE+"01",end="")
-    print(Fore.GREEN+"01",end="")
+    print(Fore.WHITE+"010",end="")
+    print(Fore.GREEN+"101",end="")
